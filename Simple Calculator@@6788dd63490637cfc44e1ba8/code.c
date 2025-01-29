a54b38aafcec96b220f34e6426900cc7992c997a
Jannat0497 <jannatkataria02@gmail.com>
2025-01-29 20:49:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,31 +3,27 @@ int main(){
     int a,b;
     scanf("%d%d",&a,&b);
     char ch;
-    int sum= a+b;
-    int diff= a-b;
-    int mul=a*b;
-    int div=a/b;
     scanf("%c",&ch);
     switch(ch){
         case '+':
-        printf("%d",sum);
-        break;
+            printf("%d",a+b);
+            break;
 
         case '-':
-        printf("%d",diff);
-        break;
+            printf("%d",a-b);
+            break;
 
         case '*':
-        printf("%d",mul);
-        break;
+            printf("%d",a*b);
+            break;
 
         case '/':
-        printf("%d",div);
-        break;
+            printf("%d",a/b);
+            break;
 
         default:
-        printf("error");
-        break;
+            printf("error");
+            break;
 
     }
     
