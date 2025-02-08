71b4e0ae4db4f8fa93dc6436769d1fc5eae8aeb7
Jannat0497 <jannatkataria02@gmail.com>
2025-02-09 01:40:33 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-void fun(int num){
-    if(num%2==0){
-        printf("1\n");
-    }
-    else{
-        printf("0\n");
-    }
-}
-int main(){
+int main() {
     int t;
-    scanf("%d",&t);
-    while(t--){
+    scanf("%d", &t);
+    
+    int results[t];
+
+    for (int i = 0; i < t; i++) {
         int num;
-        scanf("%d",&num);
-        fun(num);
+        scanf("%d", &num);
+        if (num % 2 == 0) {
+            results[i] = 1;
+        } else {
+            results[i] = 0;
+        }
+    }
+    for (int i = 0; i < t; i++) {
+        printf("%d\n", results[i]);
     }
     return 0;
 }
