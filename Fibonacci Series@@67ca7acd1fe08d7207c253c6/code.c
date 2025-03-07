c076d3d00b8b7b7fc24e7dbaaa25a3aa29b2c9c8
Jannat0497 <jannatkataria02@gmail.com>
2025-03-07 20:34:18 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+    int fib(int n) {
+        int n1=0 ,n2=1,n3;
+        if(n>0){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            printf("%d ",n3);
+            fib(n-1);
+        }
+    }
+    int main() {
+        int n;
+        scanf("%d", &n);
+        printf("0 1 ");
+        fib(n-2);
+        return 0;
+    }
