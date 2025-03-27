7608d2dff93858ce424582dc4de2a52275d473bc
Jannat0497 <jannatkataria02@gmail.com>
2025-03-27 21:07:37 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int selectionSort(int *arr){
    for (int i=0;i<n;i++){
-      for (j=0;j<n-1-i;j++){
-         if (num[j]>num[j+1]){
-            a=num[i];
-            num[j]=num[j+1];
-            num[j+1]=a;
+      for (int j=0;j<n-1-i;j++){
+         if (arr[j]>arr[j+1]){
+            int a=arr[i];
+            arr[j]=arr[j+1];
+            arr[j+1]=a;
          }
       }
    }
 }
 int printArray(int *arr){ 
    for (i=0;i<n;i++){
-      printf("%d", num[i]);
+      printf("%d", arr[i]);
    }
 }
