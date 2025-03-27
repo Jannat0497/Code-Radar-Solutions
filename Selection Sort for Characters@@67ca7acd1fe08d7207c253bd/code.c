6206f6c360c25929ff0616e4e116f89e6e2927fb
Jannat0497 <jannatkataria02@gmail.com>
2025-03-27 22:19:08 +0530

    Solution to Selection Sort for Characters

---
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void bubbleSort(char *arr,int n){
+    int flag=0;
+    for (int i=0;i<n-1;i++){
+        for (int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                flag=1;
+            }
+        }
+        if(flag==0){
+            break;
+        }
+    }
+}
+int printArray(char *arr, int n){
+    for(int i=0;i<n;i++){
+        printf("%c ",arr[i]);
+    }
+}
